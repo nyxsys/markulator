2c6703a8784aaa42f8e3f69251f5792e73c01339
nyxsys1 <mbarbera@udel.edu>
2016-05-03 04:37:54 +0000

    calculation bits sort of done

--- calc.js
+++ calc.js
@@ -1,10 +1,12 @@
 
 var current = "";
-var last = "";
+var last = null;
+var final = 0;
 var sounds= {};
+var lastOp = "";
 
 var setup = function(){
-    for(var i = 1; i <= 9; i++){
+    for(var i = 0; i <= 9; i++){
         var button = $("<button>"+ i + "</button>").addClass("number"+i);
         button.val(i)
 
@@ -14,11 +16,16 @@ var setup = function(){
             $(".buttons").append("<br>");
         }
         
-        var numAudio = document.createElement("audio");
-        numAudio.setAttribute("src","clips/"+i+".ogg");
-        sounds[i] = numAudio;
+
         
     }
+    
+    for(var n = 0; n <= 9; n++){
+        var numAudio = document.createElement("audio");
+        numAudio.setAttribute("src","clips/"+n+".ogg");
+        sounds[n] = numAudio;
+    }
+    
     $(".buttons").append("<br>");
     
     var ops = ["plus", "minus"];
@@ -44,6 +51,7 @@ var setup = function(){
     
     var errAudio = document.createElement("audio");
     errAudio.setAttribute('src','clips/boo.ogg');
+    sounds["err"] = errAudio;
 }
 
 
@@ -55,35 +63,83 @@ var numTap = function(num){
     $("#curValue").text(current);
 }
 var op = function(optype){
-    last =current;
+    calculate();
+    last = current;
     current = "";
+    lastOp = optype;
     $("#curValue").text(current);
+
 }
-
-
 var calculate = function(){
-    current = "";
-    
-    $("#curValue").text(current);
+
+    if(last){
+        if(final == 0){
+            final = parseInt(last);
+        }
+        if(lastOp == "plus"){
+            final +=  parseInt(current);
+            console.log(final)
+        }
+        else if(lastOp == "minus"){
+            final -=  parseInt(current);
+        }
+    }
 }
 
 
-$(document).ready(function(){
-    setup(); 
-    numlisteners();
-    listeners();
-
-
+var finish = function(){
+    calculate();
+    current = "";
+    final = "" + final;
     
-});
+    var i=0;
+    
+    var loop = setInterval(function(){
+        if(!isNaN(final[i])){
+        
+            console.log(final[i]);
+            sounds[final[i]].currentTime=0;
+            sounds[final[i]].play();
+            i++;
+            if(i>=final.length){
+                clearInterval(loop);
+            }
+        }
+        else{
+            sounds["err"].currentTime=0;
+            sounds["err"].play();
+            clearInterval(loop);
+        }
+        
+    },1500)
+    
+    
+    
+    
+
+    $("#curValue").text(final);
+}
+
+
 
 function listeners(){
     $(".enter").on("click", function(){
-        calculate();
-        console.log("enter");
+        finish();
+        //console.log("enter");
         sounds["enter"].currentTime = 0;
         sounds["enter"].play();
     });
+    $(".plus").on("click", function(){
+       op("plus");
+       sounds["plus"].currentTime = 0;
+       sounds["plus"].play();
+        
+    });
+    $(".minus").on("click", function(){
+        op("minus");
+        sounds["minus"].currentTime = 0;
+        sounds["minus"].play();
+    })
 }
 
 
@@ -91,58 +147,68 @@ function listeners(){
 
 function numlisteners(){
     $(".number1").on("click",function(){
-            console.log("tapped");
+            
             numTap(1);
             sounds["1"].currentTime = 0;
             sounds["1"].play();
     });
     $(".number2").on("click",function(){
-            console.log("tapped");
+            
             numTap(2);
             sounds["2"].currentTime = 0;
             sounds["2"].play();
             
     });
     $(".number3").on("click",function(){
-            console.log("tapped");
+            
             numTap(3);
             sounds["3"].currentTime = 0;
             sounds["3"].play();
     });  
     $(".number4").on("click",function(){
-            console.log("tapped");
+            
             numTap(4);
             sounds["4"].currentTime = 0;
             sounds["4"].play();
     });  
     $(".number5").on("click",function(){
-            console.log("tapped");
+            
             numTap(5);
             sounds["5"].currentTime = 0;
             sounds["5"].play();
     });  
     $(".number6").on("click",function(){
-            console.log("tapped");
+            
             numTap(6);
             sounds["6"].currentTime = 0;
             sounds["6"].play();
     });  
     $(".number7").on("click",function(){
-            console.log("tapped");
+            
             numTap(7);
             sounds["7"].currentTime = 0;
             sounds["7"].play();
     });  
     $(".number8").on("click",function(){
-            console.log("tapped");
+            
             numTap(8);
             sounds["8"].currentTime = 0;
             sounds["8"].play();
     });  
     $(".number9").on("click",function(){
-            console.log("tapped");
+            
             numTap(9);
             sounds["9"].currentTime = 0;
             sounds["9"].play();
     });  
 }
+
+
+
+
+$(document).ready(function(){
+    setup(); 
+    numlisteners();
+    listeners();
+
+});
